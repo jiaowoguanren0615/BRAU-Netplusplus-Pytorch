383bfd5825fe0fedaa2eae1aa40dd43c15c04d8b
huangshuqi <1105374939@qq.com>
2024-01-05 16:49:13 +0800

    Upload read synapse dataset & split train-test data

---
+++ split_data_list.py
@@ -0,0 +1,28 @@
+import glob
+import os
+
+
+def write_name():
+
+    if not os.path.exists('./lists/lists_Synapse'):
+        os.makedirs('./lists/lists_Synapse', exist_ok=True)
+
+    # npz files path
+    # files = glob.glob(r'E:/Synapse/Synapse/train_npz/*.npz')
+    files = glob.glob(r'E:/Synapse/Synapse/test_vol_h5/*.h5')
+    # txt文件路径
+    # f = open(r'./lists/lists_Synapse/train.txt', 'w')
+    f = open(r'./lists/lists_Synapse/test_vol.txt', 'w')
+
+    for i in files:
+        name = i.split('\\')[-1]
+
+        # TODO name = name[:-4] + '\n'   when you generate train.txt
+
+        name = name[:-7] + '\n'
+        f.write(name)
+
+    print("Finished!")
+
+
+write_name()
